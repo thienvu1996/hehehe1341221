eb2b4f7dbd06ee22b5d4a39916362d412768a926
Thien Vu <101257780+thienvu1996@users.noreply.github.com>
2026-08-26 17:21:25 +0700

    feat: show AI sharing manager on connections page

--- dashboard/app/connections/page.tsx
+++ dashboard/app/connections/page.tsx
@@ -1,5 +1,11 @@
+import { AiSharingManager } from "../../components/ai-sharing-manager";
 import { ConfigManager } from "../../components/config-manager";
 
 export default function ConnectionsPage() {
-  return <ConfigManager />;
+  return (
+    <>
+      <ConfigManager />
+      <AiSharingManager />
+    </>
+  );
 }
